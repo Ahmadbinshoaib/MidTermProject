e678f83dbc5163f11cc51a0f1fc2d743b25aba70
Ahmadbinshoaib <ahmadshoaib1044@gmail.com>
2021-04-04 15:26:57 +0500

    Employee

--- ProcurementManagementSystem/src/ProcurementManagementSystem/EmployeeLogIn.java
+++ ProcurementManagementSystem/src/ProcurementManagementSystem/EmployeeLogIn.java
@@ -110,6 +110,11 @@ public class EmployeeLogIn extends javax.swing.JFrame {
         jLabel10.setForeground(new java.awt.Color(255, 255, 255));
         jLabel10.setText("    Log In ");
         jLabel10.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
+        jLabel10.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel10MouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -199,6 +204,13 @@ public class EmployeeLogIn extends javax.swing.JFrame {
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jLabel10MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel10MouseClicked
+        // TODO add your handling code here:
+        dispose();
+        Employees a= new Employees();
+        a.setVisible(true);
+    }//GEN-LAST:event_jLabel10MouseClicked
 
     /**
      * @param args the command line arguments
